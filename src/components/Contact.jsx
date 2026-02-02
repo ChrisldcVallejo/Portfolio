630110f0c738f4f57ffd53f349a2af2d56edab74
Christian <christianldcvallejo@gmail.com>
2026-02-02 18:29:55 +0100

    Arreglado problema teléfono

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,7 +45,7 @@ export const Contact = ({ texts}) => {
 
           {/* TELÉFONO */}
           <div className="col-md-3 col-sm-6">
-            <a href="tel:+34600000000" className="text-decoration-none">
+            <a href="tel:+34666187778" className="text-decoration-none">
               <div className="card contact-card h-100 border-0 text-center p-4">
                 <i className="bi bi-whatsapp display-4 text-primary-custom mb-3"></i>
                 <h5 className="text-white">{texts.cards.phoneTitle}</h5>
